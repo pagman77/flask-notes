bb2aa5f2f91b1b468481e1acef4ff950ceee960f
Michael Paglione <pagman777@gmail.com>
2022-04-05 15:21:53 -0400

    create user, create first routes

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 """Flask app for Notes"""
-from flask import Flask, jsonify, request, render_template, redirect
+from click import password_option
+from flask import Flask, jsonify, request, render_template, redirect, session
 from flask_debugtoolbar import DebugToolbarExtension
 
-from models import db, connect_db
+from models import db, connect_db, User
+from forms import NewUserForm
 
 app = Flask(__name__)
 
@@ -18,3 +20,38 @@ app.config['SECRET_KEY'] = "I'LL NEVER TELL!!"
 # app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
+
+
+@app.get("/")
+def hompage_redirect():
+    """Redirect user to register user."""
+
+    return redirect("/register")
+
+
+@app.route('/register', methods=["GET", "POST"])
+def register_new_user():
+    """Display form, or accept submission for new user."""
+
+    form = NewUserForm()
+
+    if form.validate_on_submit():
+
+        username = form.username.data
+        password = form.password.data
+        email = form.email.data
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+
+        user = User.register(username=username, password=password, email=email,
+            first_name=first_name, last_name=last_name)
+
+        db.session.add(user)
+        db.session.commit()
+
+        session["username"] = username
+
+        return redirect("/secret")
+
+    else:
+        return render_template("/register.html", form=form)
--- models.py
+++ models.py
@@ -1,8 +1,68 @@
 """Models for Notes app."""
+from enum import unique
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
 
 bcrypt = Bcrypt()
+
+def connect_db(app):
+    """Connect this database to provided Flask app."""
+
+    db.app = app
+    db.init_app(app)
+
+
+class User(db.Model):
+    """Creates user."""
+
+    __tablename__ = "pets"
+
+    def __repr__(self):
+        """Show info about user."""
+
+        u = self
+
+        return f"<{u.first_name} {u.last_name} has username of\
+            {u.username} and email is {u.email}>"
+
+    username =      db.Column(db.String(20),
+                        primary_key=True)
+
+    password =      db.Column(db.String(100),
+                        nullable=False)
+
+    email =         db.Column(db.String(50),
+                        nullable=False,
+                        unique=True)
+
+    first_name =    db.Column(db.String(30),
+                        nullable=False)
+
+    last_name =     db.Column(db.String(30),
+                        nullable=False)
+
+    @classmethod
+    def register(cls, username, password, email, first_name, last_name):
+        """Registers the user on the class and hashes password."""
+
+        hashed = bcrypt.generate_password_hash(password).decode('utf8')
+
+        return cls(username = username, password = hashed, email= email,
+            first_name = first_name, last_name = last_name)
+
+    @classmethod
+    def authenticate(cls, username, password):
+        """Validate that user exists and has correct password."""
+
+        user = cls.query.filter_by(username=username).one_or_none()
+
+        if user and bcrypt.check_password_hash(user.password, password):
+            return user
+        else:
+            return False
+
+
+
 
